98adcd55355486f2da00aec5671c8147d9505c74
098maya <bhartimayank098@gmail.com>
2025-06-20 15:30:55 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker run -d -p 80:80 --name
+ecommerce-container
+098maya/dev-ecom-app
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run -d -p 80:80 --name ecommerce-container 098maya/dev-ecom-app
+098maya/dev-ecom-app
